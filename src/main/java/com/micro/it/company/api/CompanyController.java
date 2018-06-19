020f664ee0111b1efdbe4068fd3fb1dfe9722231
Gandhi Ramu <Gandhi_Ramu@intuit.com>
2018-06-19 15:49:27 -0700

    Added new api-get company by id.

--- src/main/java/com/micro/it/company/api/CompanyController.java
+++ src/main/java/com/micro/it/company/api/CompanyController.java
@@ -3,6 +3,7 @@ package com.micro.it.company.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,11 @@ public class CompanyController {
 	@RequestMapping("/companies")
 	public List<Company> getCompanys() {
 		return companyBusiness.getCompanies();
+	}
+	
+	@RequestMapping("/company/{id}")
+	public Company getCompany(@PathVariable String id) {
+		return companyBusiness.getCompany(id);
 	}
 
 }
--- src/main/java/com/micro/it/company/api/business/CompanyBusiness.java
+++ src/main/java/com/micro/it/company/api/business/CompanyBusiness.java
@@ -11,11 +11,18 @@ import com.micro.it.company.api.Company;
 public class CompanyBusiness {
 	private List<Company> companyList = new ArrayList<>();
 
-	//TODO will be replaced by Apache Cassandra
+	// TODO will be replaced by Apache Cassandra
 	public List<Company> getCompanies() {
 		companyList.add(new Company("1", "Google"));
 		companyList.add(new Company("2", "Facebook"));
 		return companyList;
 	}
 
+	public Company getCompany(String id) {
+
+		return companyList.stream()
+						.filter(company -> company.getCompanyId().equals(id))
+						.findFirst()
+						.orElse(null);
+	}
 }
